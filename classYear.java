41d4429d7e1b827c16f2b4f01c928826498c4924
bgould7 <49792993+bgould7@users.noreply.github.com>
2019-04-20 22:45:11 -0400

    Update classYear.java

--- classYear.java
+++ classYear.java
@@ -1,1 +1,15 @@
-
+public class classYear {
+	private int year;
+	
+	public classYear() {
+		year = 0;
+	}
+	
+	public void setYear(int y){
+		year = y;
+	}
+		
+	public int getYear() {
+		return year;
+	}
+}
