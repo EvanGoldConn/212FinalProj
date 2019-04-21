37682fd8cc2aaab0993368e43931031f5b5c3834
bgould7 <49792993+bgould7@users.noreply.github.com>
2019-04-20 22:46:15 -0400

    Update email.java

--- email.java
+++ email.java
@@ -1,1 +1,44 @@
-
+public class email {
+	private String email;
+	
+	public email() {
+		email = "";
+	}
+		
+	public String getEmail() {
+		return email;
+	}
+	
+	public int getKey() {
+		//convert email to a specific ID/ascii number
+	}
+	
+	public void displayEmail() {
+		System.out.println(email);
+	}
+	
+	public void newEmail() {
+		Scanner newE = new Scanner(System.in);
+		System.out.println("Enter a new email.");
+		email = newE.next();
+	}
+	
+	public void changeEmail() {
+		if (email == "") {
+			System.out.println("You don't have an email saved to your account.");
+			newEmail();
+		else {
+			Scanner changeE = new Scanner(System.in);
+			System.out.println("Enter your current email to change it.");
+			if (changeE == email){
+				Scanner change = new Scanner(System.in);
+				System.out.println("Change email to: ");
+				email = change.next();
+			}
+			else {
+				System.out.println("Incorrect email.");
+				changeEmail();
+			}
+		}
+	}
+}
