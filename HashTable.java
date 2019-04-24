83ac9288d2b7f854cd66a67a84954fb78c87d349
EvanGoldConn <49243207+EvanGoldConn@users.noreply.github.com>
2019-04-24 11:33:40 -0400

    Create HashTable.java

---
+++ HashTable.java
@@ -0,0 +1,36 @@
+import java.io.*;
+public class HashTable{
+	private Person[] table;
+	private int N;
+	private char[] letters;
+
+	public HashTable(){
+		N = 26;
+		table = new Person[2000];
+		letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	}
+
+	private int helper(char c){
+		for(int i = 1; i < 27; i++){
+			if(c == letters[i])
+				return i;
+		}
+
+	}
+	private int hornerHash(String s){
+		String lowerCase = s.toLowerCase();
+		int hashVal = 0;
+		for(int i = 0; i < s.length; i++){
+			hashVal = (hashVal*26+helper(lowerCase.charAt(i)))%N;
+		}
+		return hashVal;
+	}
+
+	public void addPerson(Person person){
+		int key = person.getEmailKey();
+		int hashKey = hornerHash(key);
+		table[hashKey] = person;
+
+	}
+
+}
